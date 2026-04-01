c9be7946b83b6d8f1065c32128211af452a003c9
val <249565370+GGsemka@users.noreply.github.com>
2026-04-01 15:25:44 +0300

    ляляллялялля

---
+++ Kursovichok2/Users.cs
@@ -0,0 +1,11 @@
+﻿namespace Kursovichok2
+{
+    public class Users
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    
+    }
+}
