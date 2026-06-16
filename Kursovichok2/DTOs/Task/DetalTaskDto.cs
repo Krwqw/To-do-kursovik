532e97bc190feb602bc901711eef92faa220f8d1
cl1ffur <ulanakovaleva508@gmail.com>
2026-06-16 09:26:52 +0300

    еще правки

---
+++ Kursovichok2/DTOs/Task/DetalTaskDto.cs
@@ -0,0 +1,17 @@
+﻿using Kursovichok2.DTOs.Comment;
+namespace Kursovichok2.DTOs.Task
+{
+    public class DetalTaskDto
+    {
+        //детали задачи
+        public int Id { get; set; }  //айди задачи
+        public string Title { get; set; } = string.Empty; //название
+        public string? Description { get; set; } //описание
+        public string Status { get; set; } = string.Empty; //статус
+        public DateTime? DueDate { get; set; } //дедлайн, не обязательно, если указан
+        public DateTime CreatedAt { get; set; } //дата создания
+
+        public List<VievCommDto> Comments { get; set; } = new(); /*Вложенный список комментариев, 
+                                                                  инициализация = new() создает пустой список*/
+    }
+}
